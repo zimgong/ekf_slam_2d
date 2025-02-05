66cc6e64ebcd764848a79e6b40ddd1da0f2cd30a
zim.gong <zimu.gong@lightwheel.ai>
2025-02-05 12:15:44 -0500

    first commit

---
+++ src/ekf_slam_2d_app.cpp
@@ -0,0 +1,23 @@
+
+#include "ekf_slam_ 2d/ekf_slam_2d_wrapper.h"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "ekf_slam_2d");
+  ros::NodeHandle n;
+  ros::Rate r(100);
+  EKFslamWrapper slam;
+  slam.get_param();
+  slam.init();
+  ros::Duration(3).sleep();
+
+  if (!slam.rawlogPlay())
+  {  // if not play from rawlog file
+
+    while (ros::ok())
+    {
+      ros::spinOnce();
+      r.sleep();
+    }
+  }
+}
